eaaea7cc6c4dbb55674010dc4fe3523f6d039097
NikSala10 <salamancanikol10@gmail.com>
2025-04-15 15:39:43 -0500

    Template V1

---
+++ server/db/users.db.js
@@ -0,0 +1,19 @@
+let users = [
+  {
+    id: 1,
+    name: "John Doe",
+  },
+];
+
+const getAllUsers = async () => {
+  return users;
+};
+
+const createUser = async (user) => {
+  users.push(user);
+};
+
+module.exports = {
+  getAllUsers,
+  createUser,
+};
---
+++ server/routes/users.router.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const { createUser, getUsers } = require("../controllers/users.controller");
+const router = express.Router();
+
+router.get("/users", getUsers);
+
+router.post("/users", createUser);
+
+module.exports = router;

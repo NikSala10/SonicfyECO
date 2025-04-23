56f251953413c06204e1d3ef9d36614d36ba0d49
yeliani <yelianibarbosa@gmail.com>
2025-04-23 07:24:09 -0500

    scre

--- mupi-client/screens/question1M.js
+++ mupi-client/screens/question1M.js
@@ -7,32 +7,32 @@ export default function renderScreenQuestion1M() {
       <h1>Challenge 1</h1>
       <p><strong>Your time for this 6 seconds to respond if you do not respond in the given time you will lose</p>
       <div id="sabrina">
-          <p>In what year did Sabrina Carpenter release her debut album "Eyes Wide Open"?</p>
+          <h5>In what year did Sabrina Carpenter release her debut album "Eyes Wide Open"?</h5>
       
-          <h5>A<br>2014</h5>
-          <h5>B<br>2015</h5>
-          <h5>C<br>2016</h5>
+          <p>A 2014</p>
+          <p>B 2015</p>
+          <p>C 2016</p>
       </div>
       <div id="lady">
-      <p>What was Lady Gaga's first single to reach #1 on the Billboard Hot 100?</p>
+      <h5>What was Lady Gaga's first single to reach #1 on the Billboard Hot 100?</h5>
     
-      <h5>A <br> Poker Face</h5>
-      <h5>B <br> Just Dance</h5>
-      <h5>C <br> Bad Romance</h5>
+      <p>A  Poker Face</p>
+      <p>B  Just Dance</p>
+      <p>C  Bad Romance</p>
       </div>
       <div id="shakira">
-      <p>What was Shakira's first album in English?</p>
+      <h5>What was Shakira's first album in English?</h5>
 
-      <h5>A <br> Laundry Service</h5>
-      <h5>B <br> Oral Fixation Vol. 2</h5>
-      <h5>C <br> Pies Descalzos</h5>
+      <p>A  Laundry Service</p>
+      <p>B  Oral Fixation Vol. 2</p>
+      <p>C  Pies Descalzos</p>
       </div>
       <div id="artics">
-      <p>What is the name of Arctic Monkeys' debut album released in 2006?</p>
+      <h5>What is the name of Arctic Monkeys' debut album released in 2006?</h5>
     
-      <h5>A <br> Favourite Worst Nightmare</h5>
-      <h5>B <br> Humbug</h5>
-      <h5>C <br> Whatever People Say I Am, That's What I'm Not</h5>
+      <p>A  Favourite Worst Nightmare</p>
+      <p>B  Humbug</p>
+      <p>C  Whatever People Say I Am, That's What I'm Not</p>
       </div>
       <p>On your cell phone screen, select the correct answer.</p>
       </div>
--- mupi-client/screens/question2M.js
+++ mupi-client/screens/question2M.js
@@ -2,7 +2,16 @@ export default function renderScreenQuestion1M() {
     const app = document.getElementById("app");
     app.innerHTML = `
         <div id="screen2">
-          <h2>Question2</h2>
+          <h2>Challenge 2</h2>
+          Your time for this 6 seconds to respond if you do not respond in the given time you will lose
+          <div id="sabrina-2">
+          <h5>Which of these songs is NOT on the album "Emails I Can't Send"?</h5>
+      
+          <p>A 2014</p>
+          <p>B 2015</p>
+          <p>C 2016</p>
+      </div>
+
       </div>
         `;
       // socket.on("notificar-pantalla-instructions", (data) => {

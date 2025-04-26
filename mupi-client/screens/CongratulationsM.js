b1cbadd9f8bb9d175f6fe36acc8141ca54713abc
yeliani <yelianibarbosa@gmail.com>
2025-04-26 10:15:39 -0500

    tdg

--- mupi-client/screens/CongratulationsM.js
+++ mupi-client/screens/CongratulationsM.js
@@ -6,8 +6,22 @@ export default function navigateToMupi() {
           <div id="Congratulations!">
             <h1>Congratulations!</h1>
         
+        
+        <div id="sabrina-exito">
         <h5>You won a ticket to Sabrina's next concert.</h5>
-
+        </div>
+            
+        <div id="lady-exito">
+        <h5>You won a ticket to Lady gaga's next concert.</h5>
+        </div>
+            
+        <div id="shakira-exito">
+        <h5>You won a ticket to Shakira's next concert. </h5>
+        </div>
+            
+        <div id="artic-exito">
+        <h5>You won a ticket to the next Arctic Monkeys concert.  </h5>
+        </div>
         
         <h5>Your ticket with all the information will be sent to the email address you enter.</h5>
         <p>If there are any irregularities, please contact us at spotify@gmail.com</p>

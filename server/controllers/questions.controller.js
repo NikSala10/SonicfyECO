beb65ed4b6ed8ac7c4438461920d06cf5656ae64
NikSala10 <salamancanikol10@gmail.com>
2025-05-01 16:57:04 -0500

    Render question1 for artist, check

--- server/controllers/questions.controller.js
+++ server/controllers/questions.controller.js
@@ -3,10 +3,10 @@ const { getAllQuestions } = require("../db/questions.db");
 const getQuestions = async (req, res) => {
     try {
         const questions = await getAllQuestions();
-        res.json(questions);  // Asegúrate de que el servidor responde con un JSON
+        res.send(questions); 
       } catch (error) {
         console.error("Error al obtener preguntas:", error);
-        res.status(500).json({ message: "Error al obtener las preguntas" });  // Si hay un error, devolver un mensaje adecuado
+        res.status(500).json({ message: "Error al obtener las preguntas" });
       }
 };
 

f2fb4dbfb90ceaa4e48f9097edce1cb76b9c8f14
NikSala10 <salamancanikol10@gmail.com>
2025-05-01 12:34:58 -0500

    Imgs mupi

--- mupi-client/screens/welcome.js
+++ mupi-client/screens/welcome.js
@@ -8,6 +8,7 @@ export default function renderscreenwelcome() {
         <h4>to the</h4>
         <h1>Sonicfy!</h1>
         <h4>experience!</h4>
+        <img src="https://firebasestorage.googleapis.com/v0/b/algoritmosdatos2024-02.appspot.com/o/imagesPosts%2FPORTADA.png?alt=media&token=13834b6d-214f-4f46-b9a6-fe38b5ff39ca">
         <h5Take part in this challenge!</h5>
         <p>Win a ticket to your favorite</p>
         <p>Artist's Concert</p>

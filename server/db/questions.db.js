3c3462cb41db0fe13c6c01a4216bf140982cc301
NikSala10 <salamancanikol10@gmail.com>
2025-05-01 12:26:30 -0500

    DB questions

--- server/db/questions.db.js
+++ server/db/questions.db.js
@@ -1,114 +1,106 @@
-const questions = [
-    {
-      artist: "Sabrina Carpenter",
-      questions: [
-        {
-          
-          question1: "In what year did Sabrina Carpenter release her debut album 'Eyes Wide Open'?",
-          option1: "2014",
-          option2: "2015",
-          option3: "2016",
-          correctAnswer: "2015"
-        },
-        {
-          
-          question2: "Which song by Sabrina Carpenter was featured in the Disney Channel movie 'Adventures in Babysitting'?",
-          option1: "Smoke and Fire",
-          option2: "On Purpose",
-          option3: "Wildside",
-          correctAnswer: "Wildside"
-        },
-        {
-          
-          question3: "I'm working late, 'cause I'm a singer\n(Sign)\ncome and get this pollen",
-          correctAnswer: "I'm working late, 'cause I'm a singer\nOh, he looks so cute wrapped 'round my finger\nMy twisted humor makes him laugh so often\nMy honey bee, come and get this pollen"
-        }
-      ]
-    },
-    {
-      artist: "Shakira",
-      questions: [
-        {
-         
-          question1: "Which song by Shakira was featured in the 2010 FIFA World Cup?",
-          option1: "Hips Don't Lie",
-          option2: "Waka Waka (This Time for Africa)",
-          option3: "La La La",
-          correctAnswer: "Waka Waka (This Time for Africa)"
-        },
-        {
-          
-          question2: "Shakira’s album 'Laundry Service' was released in which year?",
-          option1: "2001",
-          option2: "2002",
-          option3: "2003",
-          correctAnswer: "2001"
-        },
-        {
-          
-          question3: "Ella se hace la bruta pa' cotizarse\ncon mi tigre",
-          correctAnswer: "Ella se hace la bruta pa' cotizarse conmigo en frente\nElla se hace la gata en celo contigo\nTe cotorrea al oído pa' tenerte en alta\nElla muere por ti, tú por mí es que matas\nYo sigo tranquila, como una paloma de esquina\nMientras ella se pasa en su BM al lado mío\nYo, de aquí, no me voy\nLo que está pa' mí, ninguna va a poder quitármelo de un tirón\nYo soy loca con mi tigre"
-        }
-      ]
-    },
-    {
-      artist: "Lady Gaga",
-      questions: [
-        {
-         
-          question1: "Which Lady Gaga album features the hit song 'Poker Face'?",
-          option1: "The Fame",
-          option2: "Born This Way",
-          option3: "ARTPOP",
-          correctAnswer: "The Fame"
-        },
-        {
-         
-          question2: "What year did Lady Gaga release her debut album 'The Fame'?",
-          option1: "2007",
-          option2: "2008",
-          option3: "2009",
-          correctAnswer: "2008"
-        },
-        {
+const ante = [
+  {
+    artist: "Sabrina Carpenter",
+    questions: [
+      {
         
-          question3: "Pay the toll to the angels\n(Sign)\nIn the magic",
-          correctAnswer: "Pay the toll to the angels\nDrawin' circles in the clouds\nKeep your mind on the distance\nWhen the devil turns around\nHold me in your heart tonight\nIn the magic"
-        }
-      ]
-    },
-    {
-      artist: "Arctic Monkeys",
-      questions: [
-        {
-          
-          question1: "Which Arctic Monkeys song has the line 'Do I wanna know?'",
-          option1: "Do I Wanna Know?",
-          option2: "R U Mine?",
-          option3: "Why'd You Only Call Me When You're High?",
-          correctAnswer: "Do I Wanna Know?"
-        },
-        {
-          
-          question2: "In which year did Arctic Monkeys release their debut album?",
-          option1: "2004",
-          option2: "2005",
-          option3: "2006",
-          correctAnswer: "2006"
-        },
-        {
+        question1: "In what year did Sabrina Carpenter release her debut album 'Eyes Wide Open'?",
+        option1: "2014",
+        option2: "2015",
+        option3: "2016",
+        correctAnswer: "2015"
+      },
+      {
         
-          question3: "You got the lights\n(Sing)\ndrop coloured tongue",
-          correctAnswer: "You got the lights on in the afternoon\nAnd the nights are drawn out long\nAnd you're kissin' to cut through the gloom\nWith a cough drop coloured tongue"
-        }
-      ]
-    }
-  ];
-
-  const getAllQuestions = async () => {
-    return questions;
-  };
-  
-  module.exports = {
-    getAllQuestions,
-  };
+        question2: "Which of these songs is NOT on the album Emails I Can't Send?",
+        option1:  "because i liked a boy",
+        option2:  "Feather"  ,
+        option3: "Vicious",
+        correctAnswer: "Feather"
+      },
+      {
+        
+        question3: "You’ll see the first line of the lyrics on the screen. Your challenge is to sing the missing part out loud!",
+        correctAnswer: "I'm working late, 'cause I'm a singer\nOh, he looks so cute wrapped 'round my finger\nMy twisted humor makes him laugh so often\nMy honey bee, come and get this pollen"
+      }
+    ]
+  },
+  {
+    artist: "Shakira",
+    questions: [
+      {
+       
+        question1: "What was Shakira's first album in English?",
+        option1: "Laundry Service",
+        option2: "Oral Fixation Vol. 2",
+        option3: "Pies Descalzos",
+        correctAnswer: "Laundry Service"
+      },
+      {
+        
+        question2: "Which Shakira song became the official anthem of the 2010 World Cup?",
+        option1: "Hips Don't Lie",
+        option2: "Waka Waka",
+        option3: "La La La",
+        correctAnswer: "Waka Waka"
+      },
+      {
+        
+        question3: "You’ll see the first line of the lyrics on the screen. Your challenge is to sing the missing part out loud!",
+        correctAnswer: "Ella se hace la bruta pa' cotizarse conmigo en frente\nElla se hace la gata en celo contigo\nTe cotorrea al oído pa' tenerte en alta\nElla muere por ti, tú por mí es que matas\nYo sigo tranquila, como una paloma de esquina\nMientras ella se pasa en su BM al lado mío\nYo, de aquí, no me voy\nLo que está pa' mí, ninguna va a poder quitármelo de un tirón\nYo soy loca con mi tigre"
+      }
+    ]
+  },
+  {
+    artist: "Lady Gaga",
+    questions: [
+      {
+       
+        question1: "What was Lady Gaga's first single to reach #1 on the Billboard Hot 100?",
+        option1: "Poker Face",
+        option2: "Just Dance ",
+        option3: "Bad Romance",
+        correctAnswer: "Just Dance"
+      },
+      {
+       
+        question2: "What is the name of the Lady Gaga song that won the Oscar for Best Original Song?",
+        option1: "Always Remember Us This Way",
+        option2: "Til It Happens to You",
+        option3: "Shallow",
+        correctAnswer: "2008"
+      },
+      {
+      
+        question3: "You’ll see the first line of the lyrics on the screen. Your challenge is to sing the missing part out loud!",
+        correctAnswer: "Pay the toll to the angels\nDrawin' circles in the clouds\nKeep your mind on the distance\nWhen the devil turns around\nHold me in your heart tonight\nIn the magic"
+      }
+    ]
+  },
+  {
+    artist: "Arctic Monkeys",
+    questions: [
+      {
+        
+        question1: "What is the name of Arctic Monkeys' debut album released in 2006?",
+        option1: "Favourite Worst Nightmare",
+        option2: "Humbug",
+        option3: "Whatever People Say I Am, That's What I'm Not",
+        correctAnswer: "Whatever People Say I Am, That's What I'm Not"
+      },
+      {
+        
+        question2: "Which of these songs was the first to reach #1 in the UK?",
+        option1: "I Bet You Look Good on the Dancefloor",
+        option2: " Do I Wanna Know?",
+        option3: "R U Mine?",
+        correctAnswer: "I Bet You Look Good on the Dancefloor"
+      },
+      {
+      
+        question3: "You’ll see the first line of the lyrics on the screen. Your challenge is to sing the missing part out loud!",
+        correctAnswer: "You got the lights on in the afternoon\nAnd the nights are drawn out long\nAnd you're kissin' to cut through the gloom\nWith a cough drop coloured tongue"
+      }
+    ]
+  }
+];

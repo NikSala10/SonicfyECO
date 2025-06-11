851b951e44441a1cf1acde0de5d4e7b881c0431a
NikSala10 <salamancanikol10@gmail.com>
2025-06-11 09:28:28 -0500

    css

--- mupi-client/screens/welcome.js
+++ mupi-client/screens/welcome.js
@@ -4,14 +4,9 @@ export default function renderscreenWelcome() {
   const app = document.getElementById("app");
   app.innerHTML = `
       <div id="welcome">
-        <h2>Welcome</h2>
-        <h4>to the</h4>
-        <h1>Sonicfy!</h1>
-        <h4>experience!</h4>
-        <img src="https://firebasestorage.googleapis.com/v0/b/algoritmosdatos2024-02.appspot.com/o/imagesPosts%2FPORTADA.png?alt=media&token=13834b6d-214f-4f46-b9a6-fe38b5ff39ca">
-        <h5Take part in this challenge!</h5>
-        <p>Win a ticket to your favorite</p>
-        <p>Artist's Concert</p>
+  
+        <img src="https://firebasestorage.googleapis.com/v0/b/algoritmosdatos2024-02.appspot.com/o/imagesPosts%2FMUPI.png?alt=media&token=387a4808-96de-4540-97fb-d2ca42b47c02">
+        
 
       </div>
       `;
